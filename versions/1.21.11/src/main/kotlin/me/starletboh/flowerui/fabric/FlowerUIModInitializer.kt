b00767918d5fe9cc679bcfd57f65319b1cb64b1c
Starlet <139981691+starletboh@users.noreply.github.com>
2026-07-06 23:26:40 +0300

    Delete versions/1.21.11 directory

--- versions/1.21.11/src/main/kotlin/me/starletboh/flowerui/fabric/FlowerUIModInitializer.kt
+++
@@ -1,9 +0,0 @@
-package me.starletboh.flowerui.fabric
-
-import net.fabricmc.api.ModInitializer
-
-class FlowerUIModInitializer : ModInitializer {
-    override fun onInitialize() {
-        TODO("Not yet implemented")
-    }
-}
